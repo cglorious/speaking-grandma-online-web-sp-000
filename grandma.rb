6235d4ac53fb4da34be077f396d9cf5295de00c7
Catherine Gloria <catherinegloria@gmail.com>
2020-09-01 20:05:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,16 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+  if phrase != phrase.uppercase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.uppercase
+    return "NO, NOT SINCE 1938!"
+  else phrase == 
+    
+    
+  
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
